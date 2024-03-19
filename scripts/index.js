3c6a46d7fe4ceb6e572c185ce15329ebe537cde3
Olesya Golovacheva <AndreevaOlesyaI@yandex.ru>
2024-03-19 19:37:23 +0300

    project5

--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,32 @@
 
 // @todo: DOM узлы
 
+const cardsContainer = document.querySelector('.places__list');
+
 // @todo: Функция создания карточки
+
+function addCard(cartData, onDelete) {
+
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+
+    cardElement.querySelector('.card__title').textContent = cartData.name;
+    cardElement.querySelector('.card__image').setAttribute('src', cartData.link);
+    deleteButton.addEventListener('click', onDelete);
+    
+    return cardElement;
+};
 
 // @todo: Функция удаления карточки
 
+function handleDelete(event) {
+    event.target.closest('.card').remove();
+}
+
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach((cartData) => {
+    const card = addCard(cartData, handleDelete);
+    cardsContainer.append(card);
+});
