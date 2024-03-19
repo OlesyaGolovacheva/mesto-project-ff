79dcada191ee690dfde476f3950d5f76eb791de8
Olesya Golovacheva <AndreevaOlesyaI@yandex.ru>
2024-03-19 20:51:50 +0300

    project5_reviewe2

--- scripts/index.js
+++ scripts/index.js
@@ -6,14 +6,15 @@ const cardsContainer = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
 
-function addCard(cartData, onDelete) {
+function createCard(cardData, onDelete) {
 
     const cardTemplate = document.querySelector('#card-template').content;
     const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
     const deleteButton = cardElement.querySelector('.card__delete-button');
 
-    cardElement.querySelector('.card__title').textContent = cartData.name;
-    cardElement.querySelector('.card__image').setAttribute('src', cartData.link);
+    cardElement.querySelector('.card__title').textContent = cardData.name;
+    cardElement.querySelector('.card__image').alt = cardData.name;
+    cardElement.querySelector('.card__image').src = cardData.link;
     deleteButton.addEventListener('click', onDelete);
     
     return cardElement;
@@ -27,7 +28,7 @@ function handleDelete(event) {
 
 // @todo: Вывести карточки на страницу
 
-initialCards.forEach((cartData) => {
-    const card = addCard(cartData, handleDelete);
+initialCards.forEach((cardData) => {
+    const card = createCard(cardData, handleDelete);
     cardsContainer.append(card);
 });
